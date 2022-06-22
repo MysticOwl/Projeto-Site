03092cf2cb73e0cb443535e6f68416e9dc2b7a5f
Thiago Copello <thiaago.copello@gmail.com>
2022-06-22 15:44:27 -0300

    Drag and Drop V1.1

--- src/script/drag-and-drop.js
+++ src/script/drag-and-drop.js
@@ -3,11 +3,14 @@ function allowDropThis(i){
 }
 
 function dragThis(i){
-    i.dataTransfer.setData("action", i.target.id);
+    i.dataTransfer.setData("imagen", i.target.id);
+    console.log(i);
 }
 
 function dropThis(i){
     i.preventDefault();
-    var data = i.dataTransfer.getData("action");
-    i.target.appendChild(document.getElementById(data));
+    var imagen_id = i.dataTransfer.getData("imagen");
+    var imagen_src = document.getElementById(imagen_id).src
+    document.getElementById(i.target.id).innerHTML = `<img id="${i.target.id}" src="${imagen_src}">`;
+    console.log(imagen_src);
 }
