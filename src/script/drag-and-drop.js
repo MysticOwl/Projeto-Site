cfc01caff85efec9d83f84118b1e36c41a5e9d48
Thiago Copello <thiaago.copello@gmail.com>
2022-06-23 16:37:43 -0300

    Drag and Drop V1.1.1

--- src/script/drag-and-drop.js
+++ src/script/drag-and-drop.js
@@ -1,16 +1,29 @@
+var fluxograma_passo = 0
+//Função padrão
 function allowDropThis(i){
     i.preventDefault();
 }
-
+//Coleta todas as informações do item selecionado, com o parametro 'id'
 function dragThis(i){
-    i.dataTransfer.setData("imagen", i.target.id);
-    console.log(i);
+    // coleta o id do target
+    i.dataTransfer.setData("item_drag", i.target.id);
 }
-
+//Função para alterar quando realiza o drop
 function dropThis(i){
     i.preventDefault();
-    var imagen_id = i.dataTransfer.getData("imagen");
-    var imagen_src = document.getElementById(imagen_id).src
-    document.getElementById(i.target.id).innerHTML = `<img id="${i.target.id}" src="${imagen_src}">`;
-    console.log(imagen_src);
+    
+    // variavel para setar o id do intem segurado
+    var imagen_id = i.dataTransfer.getData("item_drag");                
+    
+    // variavel que seta a imagem do item segurado
+    var imagen_src = document.getElementById(imagen_id).src;
+    
+    // variavel que coleta o id do alvo
+    var step = document.getElementById(i.target.id);             
+    
+    // constante que seleciona a class do alvo   
+    // função para tirar as bordas do alvo
+    //step_css.style.border = 'none';
+    // função para inserir o id e classe no alvo selecionado
+    step.innerHTML = `<img id="${i.target.id}${fluxograma_passo}" src="${imagen_src}"> <div class="footerButton"><button id="remove_button_${i.target.id}${fluxograma_passo}" class="removeButton"></button><button id="add_button_${i.target.id}${fluxograma_passo}" class="addButton"></button></button></div>`
 }
