017855ca68bd0fa6e5a5dbdf248de389f9482c38
Thiago Copello <thiaago.copello@gmail.com>
2022-07-05 16:53:06 -0300

    PopUp e Form v1.0.3

--- src/script/add-remove-button.js
+++ src/script/add-remove-button.js
@@ -1,22 +1,20 @@
-var cont_steps = 0
+var cont_steps = []
 
 function creatNextStep(){
-    cont_steps++
     let divStep = document.getElementById("montagemFluxograma");
-    divStep.innerHTML += `<div id=${cont_steps} class="step borderOn" ondrop="dropThis(event)" ondragover="allowDropThis(event)"></div>`
+    cont_steps.push(`passo_${cont_steps.length}`)
+    divStep.innerHTML += `<div id=passo_${cont_steps.length} class="step borderOn" ondrop="dropThis(event)" ondragover="allowDropThis(event)"></div>`
 }
 
-function removeThisStep(step_id, numberStep,step_popup){
+function removeThisStep(step_id,step){
     let thisStep = document.getElementById(step_id.id)
-    let thisPopup = document.getElementById(step_popup.id)
     thisStep.remove()
-    thisPopup.remove()
-    let step = document.getElementById(`${numberStep}`)
     step.classList.add("borderOn")
 }
 
 function removeLastStep(){
-    let lastStep = document.getElementById(`${cont_steps}`)
+    let lastStep = document.getElementById(`passo_${cont_steps.length}`)
     lastStep.remove()
-    cont_steps--
+    cont_steps.pop()
+    console.log(cont_steps)
 }
--- src/script/drag-and-drop.js
+++ src/script/drag-and-drop.js
@@ -23,59 +23,62 @@ function dropThis(i){
 
     let classChange = document.getElementById(step_id)
     classChange.className = "step"
+
     if (image_id == "condition"){
         
         step.innerHTML = 
-        `<div id="${image_id}_passo_${step_id}">
-            <img src="${image_src}" ondblclick="popup(${image_id}_popup_${step_id})">
-                <div class="footerButton">
-                <button id="remove_button_${step_id}" class="removeButton" onclick="removeThisStep(${image_id}_passo_${step_id}, ${step_id},${image_id}_popup_${step_id})"></button>
-                </div>
-            </div>
-            <div id = "${image_id}_popup_${step_id}" class = "popupDiv popupStyle">
-            <div id = "${image_id}_closePopup_${step_id}">
+        `<div id="${step_id}_${image_id}">
+            <img src="${image_src}" ondblclick="popup(${step_id}_${image_id}_popup)">
+                <div class="footerButton"><button id="remove_button_${step_id}" class="removeButton" onclick="removeThisStep(${step_id}_${image_id},${step_id})"></button></div>
+            <div id = "${step_id}_${image_id}_popup" class = "popupDiv popupStyle"></div>
+            <div id = "${image_id}_closePopup_${step_id}"></div>
+            <div class="flexDisplay">
+
+                <div id="${step_id}_${image_id}_true" class="step borderOn"></div>
+                    <div id = "${step_id}_${image_id}_popup_true" class = "popupDiv popupStyle"></div>
+                    <div id = "${image_id}_closePopup_${step_id}"></div>   
+                <div id="${step_id}_${image_id}_false" class="step borderOn"></div>
+                <div id = "${step_id}_${image_id}_popup_false" class = "popupDiv popupStyle"></div>
+                <div id = "${image_id}_closePopup_${step_id}"></div>
+        
             </div>
         </div>`
     }
     else{
 
-        step.innerHTML = `
-        <div id="${image_id}_passo_${step_id}" ondblclick="popup(${image_id}_popup_${step_id})">
-            <img src="${image_src}">
-            <div class="footerButton">
-                <button id="remove_button_${step_id}" class="removeButton" onclick="removeThisStep(${image_id}_passo_${step_id}, ${step_id},${image_id}_popup_${step_id})"></button>
-            </div>
-        </div>
-        <div id = "${image_id}_popup_${step_id}" class = "popupDiv popupStyle">
-            <div id = "${image_id}_closePopup_${step_id}">
-            </div>
+        step.innerHTML = 
+        `<div id="${step_id}_${image_id}">
+            <img src="${image_src}" ondblclick="popup(${step_id}_${image_id}_popup)">
+                <div class="footerButton"><button id="remove_button_${step_id}" class="removeButton" onclick="removeThisStep(${step_id}_${image_id},${step_id})"></button></div>
+            <div id = "${step_id}_${image_id}_popup" class = "popupDiv popupStyle"></div>
+            <div id = "${image_id}_closePopup_${step_id}"></div>
         </div>`
 
     }
 
     switch (image_id){
         case "variavel":
-            let variavel_popup = `${image_id}_popup_${step_id}`
+            let variavel_popup = `${step_id}_${image_id}_popup`
             let var_popup = document.getElementById(variavel_popup)
             var_popup.innerHTML =`
-                <a href="#" onclick='popup(${image_id}_popup_${step_id})'>x</a>
-                <h4>${image_id}. Passo ${step_id}</h4>
+                <a class="formA" href="#" onclick='popup(${step_id}_${image_id}_popup)'>x</a>
+                <h4>${image_id}. ${step_id}</h4>
                 <forms>
                     <div>
                     <lable for="variavel_nome_${step_id}">Nome da variável:</lable>
-                    <input type="text" id="form_variavel_nome_${step_id}" name="variavel_nome_${step_id}">
+                    <input class="inputForm" type="text" id="form_variavel_nome_${step_id}" name="variavel_nome_${step_id}">
                     <lable for="variavel_valor_${step_id}">Valor: </lable>
-                    <input type="text" id="form_variavel_valor_${step_id}" name="variavel_valor_${step_id}">
+                    <input class="inputForm" type="text" id="form_variavel_valor_${step_id}" name="variavel_valor_${step_id}">
                     </div>
                 </forms>
             `
         break;
         case "input":
-            let input_popup = `${image_id}_popup_${step_id}`
+            let input_popup = `${step_id}_${image_id}_popup`
             let in_popup = document.getElementById(input_popup)
             in_popup.innerHTML =`
-                <a href="#" onclick='popup(${image_id}_popup_${step_id})'>x</a>
-                <h4>${image_id}. Passo ${step_id}</h4>
+                <a class="formA" href="#" onclick='popup(${step_id}_${image_id}_popup)'>x</a>
+                <h4>${image_id}. ${step_id}</h4>
                 <div>
                 <p><label for="input_text_${step_id}">Digite seu input:</label></p>
                 <textarea id="input_text_${step_id}" name="input_text_${step_id}" rows="4" cols=40" placeholder="Digite aqui sua mensagem de input"></textarea>
@@ -83,11 +86,11 @@ function dropThis(i){
             `
         break;
         case "output":
-            let output_popup = `${image_id}_popup_${step_id}`
+            let output_popup = `${step_id}_${image_id}_popup`
             let out_popup = document.getElementById(output_popup)
             out_popup.innerHTML =`
-                <a href="#" onclick='popup(${image_id}_popup_${step_id})'>x</a>
-                <h4>${image_id}. Passo ${step_id}</h4>
+                <a class="formA" href="#" onclick='popup(${step_id}_${image_id}_popup)'>x</a>
+                <h4>${image_id}. ${step_id}</h4>
                 <div>
                 <p><label for="output_text_${step_id}">Digite seu output:</label></p>
                 <textarea id="output_text_${step_id}" name="output_text_${step_id}" rows="4" cols=40" placeholder="Digite aqui sua mensagem de output"></textarea>
@@ -95,11 +98,11 @@ function dropThis(i){
             `
         break;
         case "condition":
-            let condition_popup = `${image_id}_popup_${step_id}`
+            let condition_popup = `${step_id}_${image_id}_popup`
             let cond_popup = document.getElementById(condition_popup)
             cond_popup.innerHTML =`
-                <a href="#" onclick='popup(${image_id}_popup_${step_id})'>x</a>
-                <h4>${image_id}. Passo ${step_id}</h4>
+                <a class="" href="#" onclick='popup(${step_id}_${image_id}_popup)'>x</a>
+                <h4>${image_id}. ${step_id}</h4>
                 <div>
                 <p><label for="condition_text_${step_id}">Digite sua condição:</label></p>
                 <textarea id="condition_text_${step_id}" name="condition_text_${step_id}" rows="4" cols=40" placeholder="Digite aqui sua condição"></textarea>
@@ -107,37 +110,20 @@ function dropThis(i){
             `
         break;
         case "loop":
-            let loop_popup = `${image_id}_popup_${step_id}`
+            let loop_popup = `${step_id}_${image_id}_popup`
             let lp_popup = document.getElementById(loop_popup)
             lp_popup.innerHTML =`
-                <a href="#" onclick='popup(${image_id}_popup_${step_id})'>x</a>
-                <h4>${image_id}. Passo ${step_id}</h4>
+                <a class="formA" href="#" onclick='popup(${step_id}_${image_id}_popup)'>x</a>
+                <h4>${image_id}. ${step_id}</h4>
                 <forms>
                     <div>
                     <lable for="loop_start_${step_id}">de:</lable>
-                    <input type="number" id="form_loop_start_${step_id}" name="loop_start_${step_id}">
+                    <input class="inputForm" type="number" id="form_loop_start_${step_id}" name="loop_start_${step_id}">
                     <lable for="loop_end_${step_id}">até: </lable>
-                    <input type="number" id="form_loop_end_${step_id}" name="loop_end_${step_id}">
+                    <input class="inputForm" type="number" id="form_loop_end_${step_id}" name="loop_end_${step_id}">
                     </div>
                 </forms>
             `            
         default:
     }
-}
-
-
-function dropInCondition(i,step_id){
-    i.preventDefault();
-
-    // variavel para setar o id do intem segurado
-    let image_id = i.dataTransfer.getData("item_drag");                
-    
-    // variavel que seta a imagem do item segurado
-    let image_src = document.getElementById(image_id).src;
-    
-    // variavel que coleta o id do alvo
-
-    let classChange = document.getElementById(step_id)
-    classChange.className = "step"
-    console.log(image_id,image_src,step_id)
 }
